15f1a99f7e0ff33845770e295b8275ab5f0b1ca9
Hamza HERBOU <hamza95herbou@gmail.com>
2020-11-21 18:57:47 +0100

    added files

---
+++ Assets/SwitchToggleUI/Scripts/SwitchToggle.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine ;
+using UnityEngine.UI ;
+using DG.Tweening ;
+
+public class SwitchToggle : MonoBehaviour {
+   [SerializeField] RectTransform uiHandleRectTransform ;
+   [SerializeField] Color backgroundActiveColor ;
+   [SerializeField] Color handleActiveColor ;
+
+   Image backgroundImage, handleImage ;
+
+   Color backgroundDefaultColor, handleDefaultColor ;
+
+   Toggle toggle ;
+
+   Vector2 handlePosition ;
+
+   void Awake ( ) {
+      toggle = GetComponent <Toggle> ( ) ;
+
+      handlePosition = uiHandleRectTransform.anchoredPosition ;
+
+      backgroundImage = uiHandleRectTransform.parent.GetComponent <Image> ( ) ;
+      handleImage = uiHandleRectTransform.GetComponent <Image> ( ) ;
+
+      backgroundDefaultColor = backgroundImage.color ;
+      handleDefaultColor = handleImage.color ;
+
+      toggle.onValueChanged.AddListener (OnSwitch) ;
+
+      if (toggle.isOn)
+         OnSwitch (true) ;
+   }
+
+   void OnSwitch (bool on) {
+      //uiHandleRectTransform.anchoredPosition = on ? handlePosition * -1 : handlePosition ; // no anim
+      uiHandleRectTransform.DOAnchorPos (on ? handlePosition * -1 : handlePosition, .4f).SetEase (Ease.InOutBack) ;
+
+      //backgroundImage.color = on ? backgroundActiveColor : backgroundDefaultColor ; // no anim
+      backgroundImage.DOColor (on ? backgroundActiveColor : backgroundDefaultColor, .6f) ;
+
+      //handleImage.color = on ? handleActiveColor : handleDefaultColor ; // no anim
+      handleImage.DOColor (on ? handleActiveColor : handleDefaultColor, .4f) ;
+   }
+
+   void OnDestroy ( ) {
+      toggle.onValueChanged.RemoveListener (OnSwitch) ;
+   }
+}
